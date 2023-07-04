b08290afc94e208e1e9e39688b36d3135b707164
girordo <tsgiroldo@gmail.com>
2023-07-04 17:43:08 -0300

    feat(main.rs): implementing a CLI port sniffer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{env, net::IpAddr, str::FromStr};
+
+struct Arguments {
+    flag: String,
+    ipaddr: IpAddr,
+    threads: u16,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        } else if args.len() > 4 {
+            return Err("too many arguments");
+        }
+        let f = args[1].clone();
+        if let Ok(ipaddr) = IpAddr::from_str(&f) {
+            return Ok(Arguments {
+                flag: String::from(""),
+                ipaddr,
+                threads: 4,
+            });
+        } else {
+            let flag = args[1].clone();
+            todo!();
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 }
